abd183e7f6c77b3e43dd459c690a05a380187dcf
ShejulShubham <148786777+ShejulShubham@users.noreply.github.com>
2025-09-22 10:49:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+      let level = document.getElementById("level");
+      depth = 0;
+
+      while(level != null){
+        depth++;
+        level = level.parentElement;
+      }
+
+      alert("The level of the element is: " + depth);
